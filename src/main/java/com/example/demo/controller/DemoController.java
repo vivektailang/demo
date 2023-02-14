31b02a33e601790926b8bf84a46c6a8a337c1cd7
Vivek <Vivek@DESKTOP-4F8R0FM>
2023-02-14 10:33:49 +0530

    push code

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@RequestMapping(path = "/testdemo",  method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> demoTest() {
+		return ResponseEntity.ok().body("Hello from demo!");
+	}
+}
